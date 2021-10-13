430cc5c7861c3402c00f755cdb1f582bad088c06
Eugene Dobry <edobry@gmail.com>
2021-10-13 14:11:30 -0400

    move control signals to stderr to enable complex commands

--- src/shell.ts
+++ src/shell.ts
@@ -10,27 +10,33 @@ type CommandResult = {
     output: string,
 };
 export type Shell = {
-    childShell: ChildProcessByStdio<internal.Writable, internal.Readable, null>,
+    childShell: ChildProcessByStdio<internal.Writable, internal.Readable, internal.Readable>,
     runCommand: ShellCommand,
     close: () => Promise<string>
 }
-export type ShellCommand = (command: string) => Promise<CommandResult>;
+export type ShellCommand = (command: string, options?: { pipeInput: boolean }) => Promise<CommandResult>;
 
 const TERMINATOR = "EOF";
 const TERMINATOR_CHUNK = `${TERMINATOR}\n`;
 
 export async function initShell(): Promise<Shell> {
-    const shell = process.env.SHELL;
+    // TODO: make configurable
+    const shell = "bash"; //process.env.SHELL;
     if(!shell)
         throw new Error("$SHELL not defined!");
 
     console.debug("launching child shell...")
-    const childShell = spawn(shell, { stdio: ["pipe", "pipe", "inherit"] });
+    const childShell = spawn(shell, [], {
+        stdio: "pipe"
+    });
 
     const chunks: string[] = [];
 
     let latestId = 0;
     const dataListeners: {
+        [index: string]: (id: string, chunk: string) => void,
+    } = {};
+    const controlListeners: {
         [index: string]: (id: string, chunk: string) => void,
     } = {};
 
@@ -41,9 +47,9 @@ export async function initShell(): Promise<Shell> {
         const chunk: string = data.toString();
 
         chunks.push(chunk);
-        // commandChunks.push(chunk);
 
-        Object.entries(dataListeners).forEach(([id, f]) => f(id, chunk));
+        Object.entries(dataListeners).forEach(
+            ([id, f]) => f(id, chunk));
 
         // console.log(`data: ${data}`)
         // console.log(`data length: ${data.length}`)
@@ -52,11 +58,29 @@ export async function initShell(): Promise<Shell> {
         // dataListeners.forEach(f => f(data));
     }).pipe(process.stdout);
 
+    childShell.stderr.on("data", (data) => {
+        const chunk: string = data.toString();
+
+        chunks.push(chunk);
+
+        Object.entries(controlListeners).forEach(
+            ([id, f]) => f(id, chunk));
+
+        // console.log(`data: ${data}`)
+        // console.log(`data length: ${data.length}`)
+        // console.log(`data end: ${data.codePointAt(data.length-1)}`)
+        // chunks.push(data);
+        // dataListeners.forEach(f => f(data));
+    })
+
     childShell.on("exit", (exitCode) => {
         // console.log(`exit: ${exitCode}`)
 
-        (exitCode == 0 ? exitListeners : errorListeners).forEach((f) =>
-            f(chunks.join(""))
+        (exitCode == 0
+            ? exitListeners
+            : errorListeners
+        ).forEach((f) =>
+            f(chunks.join(''))
         );
     });
 
@@ -65,38 +89,56 @@ export async function initShell(): Promise<Shell> {
 
         errorListeners.forEach((f) => f(err));
     });
-
     const shellClose = new Promise<string>((res, rej) => {
         exitListeners.push(res);
         errorListeners.push(rej);
     });
 
-    const runCommand: ShellCommand = async (command: string): Promise<CommandResult> => {
-        let commandChunks: string[] = [];
+    const runCommand: ShellCommand = async (command: string, options?: { pipeInput: boolean }): Promise<CommandResult> => {
+        let commandDataChunks: string[] = [];
+        let commandControlChunks: string[] = [];
 
         const commandResult = new Promise<CommandResult>((res, rej) => {
-            dataListeners[++latestId] = (id, chunk) => {
-                // console.log(`current chunk: ${chunk}`);
-                
+            const id = ++latestId;
+            dataListeners[id] = (id, chunk) => {
+                // console.log(`current data chunk: ${chunk}`);
+
+                commandDataChunks.push(chunk);
+            };
+
+            controlListeners[id] = (id, chunk) => {
+                // console.log(`current control chunk: ${chunk}`);
+                commandControlChunks.push(chunk);
+
                 if (chunk.endsWith(TERMINATOR_CHUNK)) {
-                    const exitcode = parseInt(chunk.replace(`\n${TERMINATOR_CHUNK}`, ""), 10);
-                    // console.log(exitcode);
-                    // console.log(commandChunks);
+                    // console.log("is terminator chunk")
+                    const exitcode = parseInt(
+                        commandControlChunks.join('')
+                            .replace(`\n${TERMINATOR_CHUNK}`, ''), 10);
+                    // console.log(`exit code: ${exitcode}`);
+                    // console.log(commandDataChunks);
 
                     delete dataListeners[id];
-                    res({ exitcode, output: commandChunks.join("") });
-                    commandChunks = [];
+                    delete controlListeners[id];
+                    if(options?.pipeInput)
+                        process.stdin.unpipe(childShell.stdin);
+
+                    res({ exitcode, output: commandDataChunks.join('') });
+                    commandDataChunks = [];
+                    commandControlChunks = [];
 
                     return;
                 }
-
-                commandChunks.push(chunk);
             };
         });
 
-        [command, "echo $?", `echo "${TERMINATOR}"`].forEach(x =>
+        if(options?.pipeInput) {
+            process.stdin.setRawMode(true).pipe(childShell.stdin);
+        }
+
+        [command, "echo $? >&2", `echo "${TERMINATOR}" >&2`].forEach(x =>
             childShell.stdin?.write(`${x};\n`))
-        
+
         return commandResult;
     };
 
