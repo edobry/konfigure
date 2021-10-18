262e529cc9de4a2302887d519ef4df93dfb0c910
Eugene Dobry <edobry@gmail.com>
2021-10-18 14:13:10 -0400

    dont require instance arg

--- src/commands/k9s.ts
+++ src/commands/k9s.ts
@@ -1,6 +1,6 @@
 import Command from '@oclif/command'
 
-import { commonArgs, commonFlags } from '../flags';
+import { envArg, commonFlags } from '../flags';
 import { printMode } from '../util';
 import { initEnv } from '../common';
 import { runCommand, runDtCommand } from '../shell';
@@ -13,9 +13,7 @@ export default class K9s extends Command {
        ...commonFlags
     }
     
-    static args = [
-        ...commonArgs
-    ];
+    static args = [envArg];
 
     async run() {
         const input = this.parse(K9s);
@@ -23,7 +21,7 @@ export default class K9s extends Command {
         const env = await initEnv(input);
         await env.shell.close();
 
-        const { awsAccount, k8sContext, k8sNamespace } = env.konfig.environment;
+        const { k8sContext, k8sNamespace } = env.konfig.environment;
 
         console.log(`Launching K9s in context '${k8sContext}', namespace '${k8sNamespace}'`);
         await runDtCommand(`k9s --context "${k8sContext}" --namespace "${k8sNamespace}" -c deployments`);
